1134875d5b6e6879e1214c3bdcd34aebd61262b0
Sebastian <105845881+SebastianCoding@users.noreply.github.com>
2023-11-23 19:47:03 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,138 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Wofür steht HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hype Text Makeup Language",
+      "Hyper Text Markup Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Welche Programmiersprache wird oft für die Webentwicklung verwendet?",
+    answer: "JavaScript",
+    options: [
+    "Java",
+    "Joomla",
+    "JavaScript",
+    "Jupyter"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Was ist ein Byte?",
+    answer: "8 Bits",
+    options: [
+    "4 Bits",
+    "16 Bits",
+    "8 Bits",
+    "12 Bits"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Welches Dateiformat wird für Bilder mit transparentem Hintergrund verwendet?",
+    answer: "PNG",
+    options: [
+    "JPEG",
+    "GIF",
+    "PNG",
+    "TIFF"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Was ist ein Betriebssystem?",
+    answer: "Software zur Verwaltung von Hardware und Anwendungen",
+    options: [
+    "Software zur Bildbearbeitung",
+    "Hardwarekomponente eines Computers",
+    "Software zur Verwaltung von Hardware und Anwendungen",
+    "Webbrowser"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Was versteht man in einer Behörde unter einem Referat?",
+    answer: "Eine Organisationseinheit",
+    options: [
+    "Der Name eines Angestellten",
+    "Ein Vortrag in der Berufsschule",
+    "Eine Organisationseinheit",
+    "Ein Nachtrag im Gleitzeitjournal"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Was versteht man unter einem Intranet?",
+    answer: "Interne Homepage eines Unternehmen/Behörde",
+    options: [
+    "Anderes Wort für Internet",
+    "Interne Homepage eines Unternehmen/Behörde",
+    "Das Organigramm eines Unternehmens",
+    "Das interne Telefonverzeichnis"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Die Bundesnetzagentur ist..",
+    answer: "eine Regulierungsbehörde",
+    options: [
+    "ein Unternehmen",
+    "ein Telekomunikationsanbieter",
+    "ein IT-Unternehmen",
+    "eine Regulierungsbehörde"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Was ist ein dualer Ausbildungsberuf?",
+    answer: "praktischer Arbeit und theoretischem Unterricht",
+    options: [
+    "Beruf mit zwei Tätigkeitsfeldern",
+    "Ausbildung nur im Betrieb",
+    "Beruf mit doppelter Arbeitsbelastung",
+    "praktischer Arbeit und theoretischem Unterricht"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Unterschied Ausbildung und Studium?",
+    answer: "Ausbildung: Praxisorientiert, Studium: Theoretisch",
+    options: [
+    "Ausbildung nur für handwerkliche Berufe",
+    "Ausbildung schneller als Studium",
+    "Studium beinhaltet immer praktische Phase",
+    "Ausbildung: Praxisorientiert, Studium: Theoretisch"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Was bedeutet Netto-Gehalt?",
+    answer: "Gehalt nach allen Abzügen",
+    options: [
+    "Gehalt vor allen Abzügen",
+    "Gehalt vor Steuerabzug",
+    "Gehalt nach allen Abzügen",
+    "Gehalt vor Sozialversicherungsabzug"
+    ]
+    },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
